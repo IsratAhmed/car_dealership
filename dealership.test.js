e1e8ad503f3c5efb9090472abe2cfc7b8d9aec61
Israt Ahmed <110242882+IsratAhmed@users.noreply.github.com>
2022-09-23 15:07:26 +0100

    Update dealership.test.js

--- dealership.test.js
+++ dealership.test.js
@@ -16,6 +16,7 @@ test('it should have a number of cars', () => {
 })
 
 test("Car stock", () => {
+
     let name = "carStock";
     let carNumber = 2;
     let carStock= [new Car('Ford', 25000, 'Diesel'), new Car('Toyota', 60000, 'Petrol')];
@@ -26,7 +27,42 @@ test("Car stock", () => {
 
     const actual = dealership.countCarStock();
     const expected = 2;
-    
     expect(actual).toBe(expected);
    
+});
+
+test("Add car stock", () => {
+    let addCarStock = [new Car('Citroen', 7800, 'Diesel'), new Car('Mercedes', 100000, 'Petrol')];
+
+    let dealership = new Dealership('carStock', 3, 3);
+
+    dealership.member = member;
+
+    dealership.addCarStock(addCarStock);
+
+    const actual = dealership.countCarStock();
+    const expected = 1;
+    expect(actual).toBe(expected);
+    
+});
+
+test("Find by manufacturer's name", () => {
+    let car = new Car('Ford', 25000, 1);
+
+    member.car = car;
+
+    const actual = member.manufacturerName();
+    const expected = "Ford";
+    expect(actual).toBe(expected);
+
+});
+
+test("Find car from a given manufacturer", () => {
+    let car = new Car('Ford', 25000, 1);
+
+    member.car = car;
+
+    const actual = member.arrayCarManufacturer("Ford").length;
+    const expected = 1;
+    expect(actual).toBe(expected);
 });
