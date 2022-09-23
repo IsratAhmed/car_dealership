a4674822efcc676fe2bade136c29e027aff1e527
Israt Ahmed <110242882+IsratAhmed@users.noreply.github.com>
2022-09-23 10:25:50 +0100

    Update dealership.test.js

--- dealership.test.js
+++ dealership.test.js
@@ -1,4 +1,5 @@
 const Dealership = require('./dealership')
+const Car = require('./car')
 
 let member;
 
@@ -13,3 +14,19 @@ test('it should have a name', () => {
 test('it should have a number of cars', () => {
     expect(member.carNumber).toBe(12);
 })
+
+test("Car stock", () => {
+    let name = "carStock";
+    let carNumber = 2;
+    let carStock= [new Car('Ford', 25000, 'Diesel'), new Car('Toyota', 60000, 'Petrol')];
+    
+    let dealership = new Dealership('carStock', 2, 1);
+   
+    dealership.carStock = carStock;
+
+    const actual = dealership.countCarStock();
+    const expected = 2;
+    
+    expect(actual).toBe(expected);
+   
+});
