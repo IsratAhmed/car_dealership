bf9d72e311f26cbd60715ec3e5e0d7ccd676023a
Israt Ahmed <110242882+IsratAhmed@users.noreply.github.com>
2022-09-23 10:16:19 +0100

    Update dealership.js

--- dealership.js
+++ dealership.js
@@ -4,44 +4,44 @@ const Dealership = function(name, carNumber){
     this.carStock = [];
 }
 
-// Dealership.prototype.carStock = function(){
-//     console.log("There are "+ this.carStock.length + "in stock.");
-// }
+Dealership.prototype.carStock = function(){
+    console.log("There are "+ this.carStock.length + "in stock.");
+}
 
-// Dealership.prototype.addCarStock  = function (car) {                
-//     if(this.carStock.length<this.carNumber){
-//         this.carStock.push(car)
-//         console.log("The car has been added."); 
-//     } else {
-//         console.log("The shop is full.");
-//     }
+Dealership.prototype.addCarStock  = function (car) {                
+    if(this.carStock.length<this.carNumber){
+        this.carStock.push(car)
+        console.log("The car has been added."); 
+    } else {
+        console.log("The shop is full.");
+    }
     
-// } 
+} 
 
-// Dealership.prototype.arrayCarManufacturer = function() {
-//     let arrayCarManufacturer = [];
-//     let carStock = this.carStock;
+Dealership.prototype.arrayCarManufacturer = function() {
+    let arrayCarManufacturer = [];
+    let carStock = this.carStock;
 
-//     for(i in carStock){
-//         arrayCarManufacturer.push(carStock[i].name);
-//     }
+    for(i in carStock){
+       arrayCarManufacturer.push(carStock[i].name);
+    }
 
-//     console.log("Array: " + arrayCarManufacturer);
-// }
+    console.log("Array: " + arrayCarManufacturer);
+}
 
-// Dealership.prototype.name = function(name) {
-//     const message = `${this.name} is the manufacturer of the cars.`
-//     console.log(message);
-// }
+Dealership.prototype.name = function(name) {
+    const message = `${this.name} is the manufacturer of the cars.`
+    console.log(message);
+}
 
-// Dealership.prototype.maxValue = function() {
+Dealership.prototype.maxValue = function() {
 
-//     const value = 0;
+    const value = 0;
 
-//     const increasingAmount = this.carStock.map(Element => Element.price);
+    const increasingAmount = this.carStock.map(Element => Element.price);
 
-//     return increasingAmount.reduce((previousValue, currentValue) => previousValue + currentValue, value);
+    return increasingAmount.reduce((previousValue, currentValue) => previousValue + currentValue, value);
 
-// }
+}
 
 module.exports = Dealership;
